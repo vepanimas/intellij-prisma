d941baa10a5b583638de0015323429caa9a09b76
GitHub Action <action@github.com>
2022-08-22 02:00:38 +0000

    Template cleanup

--- src/main/kotlin/com/github/vepanimas/intellijprisma/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/vepanimas/intellijprisma/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.vepanimas.intellijprisma.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.vepanimas.intellijprisma.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/vepanimas/intellijprisma/services/MyProjectService.kt
+++ src/main/kotlin/com/github/vepanimas/intellijprisma/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.vepanimas.intellijprisma.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.vepanimas.intellijprisma.MyBundle
 
 class MyProjectService(project: Project) {
 
